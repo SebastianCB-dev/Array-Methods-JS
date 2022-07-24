907e3125864ce5e29967c6fb3f5506d4ce8e6405
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-07-23 19:48:45 -0500

    Feat: Added join method

---
+++ src/join.js
@@ -0,0 +1,9 @@
+console.clear();
+const countryList = ['England', 'Canada', 'Colombia'];
+
+// Join method creates a string with the elements from an array.
+// Comma is the separate by default
+
+const res = countryList.join(', ')
+
+console.log('I want to visit:', res);
