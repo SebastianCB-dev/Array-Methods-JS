a8d870d0c977a3ba31cb5c58b1ba9e459e4b3df4
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-07-25 17:33:17 -0500

    Feat: Added unshift  method

---
+++ src/unshift.js
@@ -0,0 +1,9 @@
+console.clear();
+const numbers = [1, 2, 3];
+
+// Add the new elements at the beggining of the array.
+
+const newLengthArray = numbers.unshift(0, -1, -2, -3);
+
+console.log('Array:', numbers);
+console.log('New Length Array:', newLengthArray);
