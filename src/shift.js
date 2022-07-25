b35d93014a852620f52f7da7c1d6daba95e48d46
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-07-25 17:36:07 -0500

    Feat: Added shift method

---
+++ src/shift.js
@@ -0,0 +1,9 @@
+console.clear();
+const numbers = [1, 2, 3];
+
+// Delete the first element of an array -> Opossite of pop
+
+const deletedElement = numbers.shift();
+
+console.log('Array:', numbers);
+console.log('Deleted Element:', deletedElement);
