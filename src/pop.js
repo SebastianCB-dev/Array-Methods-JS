db8b4550628ad1ba115b9c6744890c3025eb73c7
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-07-25 17:28:48 -0500

    Feat: Added pop method

---
+++ src/pop.js
@@ -0,0 +1,7 @@
+console.clear();
+const numbers = [1, 2, 3];
+
+const lastElementDeleted = numbers.pop();
+
+console.log('Array:', numbers);
+console.log('Last element Deleted:', lastElementDeleted);
