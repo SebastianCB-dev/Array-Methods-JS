e14b418ff92d34570c3c990359c6421ed7062634
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-07-28 08:33:13 -0500

    Feat: Added array isArray method

---
+++ src/isArray.js
@@ -0,0 +1,9 @@
+console.clear();
+
+const names = ['Florin', 'Ivan', 'Liam'];
+const str = 'Hello World!';
+const number = 18;
+
+console.log(Array.isArray(names));
+console.log(Array.isArray(str));
+console.log(Array.isArray(number));
